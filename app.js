0c4554ef48c9ff5cbadc07c421ff899bbe8411b3
Jose Tapia <jose.tapia@ki.se>
2017-11-21 11:34:09 +0100

    Express Session Part 1

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -36,12 +38,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if (!req.signedCookies.user){
+  if (!req.session.user){
     var authHeader = req.headers.authorization;
     
     if (!authHeader) {
@@ -56,7 +65,7 @@ function auth(req, res, next) {
     var password = auth[1];
   
     if (username === 'admin' && password === 'password') {
-      res.cookie('user', 'admin', { signed : true });
+      req.session.user = 'admin';
       next();
     }
     else {
@@ -67,7 +76,7 @@ function auth(req, res, next) {
     }
   }
   else {
-    if (req.signedCookies.user === 'admin') {
+    if (req.session.user === 'admin') {
       next();
     }
     else {
