ded2013e92eb85e97e46a9f010ee42949010537f
Jose Tapia <jose.tapia@ki.se>
2017-11-22 16:46:30 +0100

    Assignment 3 - User Authentication

--- authenticate.js
+++ authenticate.js
@@ -12,8 +12,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 exports.getToken = function(user) {
-    return jwt.sign(user, config.secretKey,
-        {expiresIn: 3600});
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
 };
 
 var opts = {};
@@ -35,3 +34,14 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => 
 }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function(req, res, next) {
+    if (req.user.admin === false) {
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        next(err);
+    }
+    else {
+        next();
+    }
+};
